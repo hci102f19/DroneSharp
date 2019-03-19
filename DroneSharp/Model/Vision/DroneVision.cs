637c6a7ddd2dfb4f08612b88fbed3c3bb6aa076c
SDCAAU <bstaffe@gmail.com>
2019-03-19 15:40:10 +0100

    Added drone/bebopVision classes

---
+++ DroneSharp/Model/Vision/DroneVision.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DroneSharp.Model.Vision
+{
+    public class DroneVision
+    {
+        public LineProcessing Canny { get; set; }
+        public FrameBuffer FrameBuffer { get; set; }
+
+    }
+}
